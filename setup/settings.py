2de5f1b9d6a1928e441a0048b77cb99b53c0fb14
lilcoded7 <lilcoded7@gmail.com>
2024-08-28 02:32:27 +0100

    cors updated

--- setup/settings.py
+++ setup/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -81,6 +82,10 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:46171",
+    "http://localhost:46171",
+]
 
 
 # Database
